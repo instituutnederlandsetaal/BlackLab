5dfe04a196cb1a664bd2421ba76826a5bf73d13b
KCMertens <koen.mertens@ivdnt.org>
2025-11-18 17:08:31 +0100

    Speedup: retrieve used pids only once during indexing
    Does mean we need to keep our own list in check, and it would break if there's concurrent IndexWriters.
    However BlackLab always has a singleton IndexWriter for a given index.

--- engine/src/main/java/nl/inl/blacklab/index/BLIndexWriterProxyLucene.java
+++ engine/src/main/java/nl/inl/blacklab/index/BLIndexWriterProxyLucene.java
@@ -2,23 +2,33 @@ package nl.inl.blacklab.index;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.DocumentStoredFieldVisitor;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TermQuery;
 
 import nl.inl.blacklab.exceptions.ErrorIndexingFile;
 import nl.inl.blacklab.search.BlackLabIndexWriter;
 import nl.inl.blacklab.search.indexmetadata.MetadataField;
 import nl.inl.util.StringUtil;
 
+import org.jspecify.annotations.Nullable;
+
 /**
  * Simple proxy for Lucene IndexWriter.
  */
 public class BLIndexWriterProxyLucene implements BLIndexWriterProxy, Closeable {
+    private boolean pidsInitialized = false;
+    private ObjectOpenHashSet<String> usedPids = null;
 
     private final BlackLabIndexWriter index;
 
@@ -34,47 +44,50 @@ public class BLIndexWriterProxyLucene implements BLIndexWriterProxy, Closeable {
 
     synchronized String getPidFieldName() {
         if (pidFieldName == null) {
+
             MetadataField pidField = index.metadata().metadataFields().pidField();
             pidFieldName = pidField == null ? null : pidField.name();
         }
         return pidFieldName;
     }
 
+    /** Get the PID for this document, if the index has a PID field. */
+    private @Nullable Term getPid(BLInputDocument document) {
+        String pidFieldName = getPidFieldName();
+        if (pidFieldName == null)
+            return null;
+        String pid = document.get(pidFieldName);
+        if (pid == null) {
+            throw new ErrorIndexingFile("Document has no persistent identifier (pidField '" + pidFieldName + "'). Document: " + document);
+        }
+        String desensitizedPid = StringUtil.desensitize(pid); // lowercase, remove accents
+        return new Term(pidFieldName, desensitizedPid);
+    }
+
+    private enum Action { ADD, UPSERT, SKIP, FAIL }
+    private synchronized Action getActionForPid(@Nullable Term pid) {
+        if (pid == null) return Action.ADD;
+
+        var usedPids = getUsedPids();
+        if (!usedPids.add(pid.text())) { // is not a new entry in the set
+            return switch(index.getIfDocumentExists()) {
+                case UPSERT -> Action.UPSERT;
+                case SKIP -> Action.SKIP;
+                case FAIL -> Action.FAIL;
+            };
+        }
+        return Action.ADD;
+    }
+
     @Override
     public void addDocument(BLInputDocument document) throws IOException {
-        String pidFieldName = getPidFieldName();
-        if (pidFieldName != null) {
-            // Index has a persistent identifier.
-            String pid = document.get(pidFieldName);
-            if (pid == null) {
-                // pidField (persistent identifier) must be specified
-                throw new ErrorIndexingFile("Document has no persistent identifier (pidField '" + pidFieldName +
-                        "'). Document: " + document);
-            }
-            // See if a document with this pid already exists
-            try (DirectoryReader reader = DirectoryReader.open(indexWriter)) {
-                String desensitizedPid = StringUtil.desensitize(pid); // lowercase, remove accents
-                Term pidTerm = new Term(pidFieldName, desensitizedPid);
-                int n = reader.docFreq(pidTerm);
-                if (n > 0) {
-                    switch (index.getIfDocumentExists()) {
-                    case UPSERT ->
-                        // Document with this pid already exists; delete it first so we can add the new version
-                            indexWriter.deleteDocuments(new TermQuery(pidTerm));
-                    case SKIP -> {
-                        // Document with this pid already exists; skip this one
-                        return;
-                    }
-                    case FAIL ->
-                        // Document with this pid already exists; fail
-                            throw new ErrorIndexingFile(
-                                    "Document with pid '" + pid + "' already exists in index; cannot add document: "
-                                            + document);
-                    }
-                }
-            }
-        }
-        indexWriter.addDocument(luceneDoc(document));
+        Term pid = getPid(document);
+        switch (getActionForPid(pid)) {
+        case ADD -> { indexWriter.addDocument(luceneDoc(document)); }
+        case UPSERT -> { indexWriter.updateDocument(pid, luceneDoc(document)); }
+        case SKIP -> { /* do nothing */ }
+        case FAIL ->  { throw new ErrorIndexingFile("Document with pid '" + pid + "' already exists in index; cannot add document: " + document); }
+        };
     }
 
     private Document luceneDoc(BLInputDocument document) {
@@ -118,5 +131,47 @@ public class BLIndexWriterProxyLucene implements BLIndexWriterProxy, Closeable {
     @Override
     public int getNumberOfDocs() {
         return indexWriter.getDocStats().numDocs;
+    }
+
+    private Set<String> getUsedPids() {
+        if (!pidsInitialized) {
+            usedPids = new ObjectOpenHashSet<>();
+            String pidFieldName = getPidFieldName();
+            if (pidFieldName != null) {
+                try (IndexReader reader = DirectoryReader.open(indexWriter)) {
+                    var fields = reader.storedFields();
+                    var fieldsToVisit = Collections.singleton(pidFieldName);
+
+                    for (int i = 0; i < reader.maxDoc(); i++) {
+                        var visitor = new DocumentStoredFieldVisitor(fieldsToVisit) {
+                            private boolean found = false;
+
+                            @Override
+                            public Status needsField(FieldInfo fieldInfo) {
+                                if (found)
+                                    return Status.STOP;
+                                if (fieldsToVisit.contains(fieldInfo.name)) {
+                                    found = true;
+                                    return Status.YES;
+                                }
+                                return Status.NO;
+                            }
+                        };
+                        fields.document(i, visitor);
+                        var doc = visitor.getDocument();
+
+                        String pid = doc.get(pidFieldName);
+                        if (pid != null) {
+                            usedPids.add(pid);
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new ErrorIndexingFile("Error gathering existing persistent identifiers from index", e);
+                }
+            }
+        }
+
+        pidsInitialized = true;
+        return usedPids;
     }
 }
