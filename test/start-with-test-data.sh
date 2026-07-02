b847e95dc50f7768be7874272118bc6a5227eef0
KCMertens <koen.mertens@ivdnt.org>
2026-07-02 14:09:37 +0200

    Minor integration test harness improvements
    - add a retry/timeout loop around waiting for BLS to come online
    - skip unit tests when building blacklab image during integration tests

--- test/start-with-test-data.sh
+++ test/start-with-test-data.sh
@@ -10,13 +10,26 @@ mkdir /data/index
 mkdir /data/user-index
 
 cd /usr/local/lib/blacklab-tools || exit
+
+run_index_tool() {
+    label="$1"
+    shift
+    log="/tmp/blacklab-index-${label}.log"
+    echo "Indexing ${label}..."
+    if ! java -cp '*' nl.inl.blacklab.tools.IndexTool "$@" > "${log}" 2>&1; then
+        echo "Indexing ${label} failed. Log follows:"
+        cat "${log}"
+        exit 1
+    fi
+}
+
 # NOTE: we intentionally add the documents in this order, so hits are not automatically sorted by document pid.
 #       this way we actually test the sort operation.
-java -cp '*' nl.inl.blacklab.tools.IndexTool create /data/index/test '/test-data/input/PBsve435.xml' voice-tei
-java -cp '*' nl.inl.blacklab.tools.IndexTool add    /data/index/test '/test-data/input/PBsve430.xml' voice-tei
-java -cp '*' nl.inl.blacklab.tools.IndexTool add    /data/index/test '/test-data/input/PRint602.xml' voice-tei
+run_index_tool test-1 create /data/index/test '/test-data/input/PBsve435.xml' voice-tei
+run_index_tool test-2 add    /data/index/test '/test-data/input/PBsve430.xml' voice-tei
+run_index_tool test-3 add    /data/index/test '/test-data/input/PRint602.xml' voice-tei
 
 # Small parallel corpus for direct BLS response regression tests.
-java -cp '*' nl.inl.blacklab.tools.IndexTool create /data/index/parallel '/test-data/parallel/minimal-parallel.xml' '/test-data/parallel/minimal-parallel.blf.yaml'
+run_index_tool parallel create /data/index/parallel '/test-data/parallel/minimal-parallel.xml' '/test-data/parallel/minimal-parallel.blf.yaml'
 #cd /usr/local/tomcat && catalina.sh jpda run
 cd /usr/local/tomcat && catalina.sh run
