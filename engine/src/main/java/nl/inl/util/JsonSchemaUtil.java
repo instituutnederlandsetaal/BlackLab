44893886dff0819684f9f5b8ad2a1638e23a3616
Koen Mertens <koen.mertens@ivdnt.org>
2026-07-14 13:47:26 +0200

    Fix missing definitions in input-format json schema (#665)
    Map values would degrade to type "object"

--- engine/src/main/java/nl/inl/util/JsonSchemaUtil.java
+++ engine/src/main/java/nl/inl/util/JsonSchemaUtil.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Type;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.victools.jsonschema.generator.Option;
 import com.github.victools.jsonschema.generator.OptionPreset;
 import com.github.victools.jsonschema.generator.SchemaGenerator;
 import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
@@ -25,6 +26,7 @@ public class JsonSchemaUtil {
             ObjectMapper mapper = new ObjectMapper();
             SchemaGeneratorConfigBuilder configBuilder =
                     new SchemaGeneratorConfigBuilder(mapper, SchemaVersion.DRAFT_2020_12, OptionPreset.PLAIN_JSON);
+            configBuilder.with(Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES);
             configBuilder.with(new JacksonModule()); // honor Jackson annotations
             SchemaGenerator generator = new SchemaGenerator(configBuilder.build());
             JsonNode schema = generator.generateSchema(targetType);
