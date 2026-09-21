8bd32616ad35a6472079550b29ccc415085204dd
Jan Niestadt <jan.niestadt@ivdnt.org>
2026-09-21 10:12:11 +0200

    Comments on ClauseCombiner.

--- engine/src/main/java/nl/inl/blacklab/search/lucene/optimize/ClauseCombiner.java
+++ engine/src/main/java/nl/inl/blacklab/search/lucene/optimize/ClauseCombiner.java
@@ -11,10 +11,37 @@ public abstract class ClauseCombiner {
 
     public static final int CANNOT_COMBINE = Integer.MAX_VALUE;
 
+    /**
+     * Determine the priority of combining the two clauses. Lower numbers are higher priority.
+     *
+     * Returns CANNOT_COMBINE if they can't be combined.
+     *
+     * @param left left clause
+     * @param right right clause
+     * @param reader index reader
+     * @return priority of combining the two clauses
+     */
     public abstract int priority(BLSpanQuery left, BLSpanQuery right, IndexReader reader);
 
+    /**
+     * Combine the two clause's using our optimization strategy. Return null if we can't combine them.
+     *
+     * @param left left clause
+     * @param right right clause
+     * @param reader index reader
+     * @return combined clause
+     * @throws UnsupportedOperationException if the clauses can't be combined
+     */
     public abstract BLSpanQuery combine(BLSpanQuery left, BLSpanQuery right, IndexReader reader);
 
+    /**
+     * Check if the two clauses can be combined using our optimization strategy.
+     *
+     * @param left left clause
+     * @param right right clause
+     * @param reader index reader
+     * @return true if they can be combined, false if not
+     */
     public boolean canCombine(BLSpanQuery left, BLSpanQuery right, IndexReader reader) {
         return priority(left, right, reader) != CANNOT_COMBINE;
     }
